4c89f305de88c3e28f3191c917d7a639a74704e1
FrigoAlex <alejo012903@hotmail.com>
2023-02-16 22:38:27 -0500

    🔧 Add API_URL and API_KEY

---
+++ assets/js/utils/constants.js
@@ -0,0 +1,5 @@
+// -------------- API CONSTANTS ----------------
+export const API_URL = "https://content.guardianapis.com/search";
+export const API_KEY = "9ac6c3a1-6a08-4333-8281-b8971939e3ce";
+
+
