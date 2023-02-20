ee108e48cbf0ba1721aa83cc3cb5422f5dc02667
FrigoAlex <alejo012903@hotmail.com>
2023-02-19 20:24:38 -0500

    🔧 Add declaration and export for DOM elements

---
+++ assets/js/utils/inputElements.js
@@ -0,0 +1,4 @@
+// -------------- DOM ELEMENTS ----------------
+export const [lang] = document.getElementsByName("Language");
+export const [pageSize] = document.getElementsByName("Page-Size");
+export const [q] = document.getElementsByName("search");
