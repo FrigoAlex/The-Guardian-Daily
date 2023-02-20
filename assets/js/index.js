adf23124c581e8f7f8be69c748e432a33f2714b5
FrigoAlex <alejo012903@hotmail.com>
2023-02-19 21:13:02 -0500

    ♻️ Refactor and optimization to the index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,15 @@
 import { Noticia } from "./utils/classnews.js";
-import { renderNews } from "./utils/render.js";
+import { reactiveNews } from "./utils/render.js";
+import "./utils/filters.js";
+import {saveStorage, isStore} from "./utils/store.js";
 
-const getNews = await Noticia.fetchNoticias({
-    lang: "",
-    q: "",
-    "page-size": 10,
+
+const params = {
+    lang: isStore("lang")||"",
+    q: isStore("q")||"",
+    "page-size": Number(isStore("page-size")||10),
     page: 1,
-});
-    const newsElements = getNews.map((news) => {
-        return renderNews(news);
-});
-const newsCont = document.querySelector(".news-cont");
-console.log(newsCont)
-newsElements.forEach(element => {
-    newsCont.appendChild(element);
-});
+};
+saveStorage(params);
+
+reactiveNews((await Noticia.fetchNoticias(params)).noticias);
