6c1dd14d044d0e94ae6ce15849f7a831643c47c4
FrigoAlex <alejo012903@hotmail.com>
2023-02-20 21:16:06 -0500

    🎨Add debounce timer to constants

--- assets/js/utils/constants.js
+++ assets/js/utils/constants.js
@@ -1,5 +1,6 @@
 // -------------- API CONSTANTS ----------------
 export const API_URL = "https://content.guardianapis.com/search";
 export const API_KEY = "9ac6c3a1-6a08-4333-8281-b8971939e3ce";
-
+// -------------- Debounce timer ----------------
+export const debounceTimer = 1000;
 
