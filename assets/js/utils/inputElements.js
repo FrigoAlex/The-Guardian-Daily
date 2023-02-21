b2bf5f75e9b06dee74286b2818ae3c21426c018d
FrigoAlex <alejo012903@hotmail.com>
2023-02-20 22:06:56 -0500

    🔧 Add new HTML selectors

--- assets/js/utils/inputElements.js
+++ assets/js/utils/inputElements.js
@@ -6,3 +6,5 @@ export const [q] = document.getElementsByName("search");
 export const arrowLeft = document.getElementById("prev-btn");
 export const arrowRight = document.getElementById("next-btn");
 export const pagElementsUl = document.querySelector(".pag-elements-ul");
+export const loaderCont = document.querySelector(".loader-cont");
+export const newsCont = document.querySelector(".news-cont");
